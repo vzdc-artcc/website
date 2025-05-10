a007066c8990dfe4c5c4ca7331e9aa77fefd1c92
Aneesh Reddy <aneeshjreddy@gmail.com>
2025-05-10 14:33:14 -0400

    Exclude people who have a trainer request from purge assistant.

--- app/admin/purge-assistant/page.tsx
+++ app/admin/purge-assistant/page.tsx
@@ -36,6 +36,7 @@ export default async function Page(
             controllerStatus: {
                 not: "NONE",
             },
+            trainingAssignmentRequestStudent: null,
             ...(includeLoas === 'false' && {
                 OR: [
                     {
