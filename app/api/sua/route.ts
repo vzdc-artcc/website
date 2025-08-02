30abe203acc2fbb890516ddcd6488cadb3c87e1a
Aneesh Reddy <aneeshjreddy@gmail.com>
2025-08-01 22:09:20 -0400

    Update route to filter events starting from now to the next 2 hours

--- app/api/sua/route.ts
+++ app/api/sua/route.ts
@@ -21,7 +21,8 @@ export async function GET() {
         },
         where: {
             start: {
-                lte: new Date(Date.now() - 2 * 60 * 60 * 1000), // 2 hours ago
+                gte: new Date(),
+                lte: new Date(Date.now() + 2 * 60 * 60 * 1000), // 2 hours ago
             },
         },
         omit: {
