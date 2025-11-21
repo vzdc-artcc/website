d8c305a21ca3d53ee922a8a5a26b20c60a926261
Aneesh Reddy <75503096+beabravedude@users.noreply.github.com>
2025-11-20 23:55:33 -0500

    Remove redundant logging in verifyUpdaterKey for cleaner output

--- lib/update.ts
+++ lib/update.ts
@@ -6,13 +6,6 @@ const {UPDATER_KEY} = process.env;
 
 export const verifyUpdaterKey = async (req: NextRequest) => {
     const apiKey = req.headers.get('API-Key');
-    // print all headers
-    console.log('Request Headers:');
-    req.headers.forEach((value, key) => {
-        console.log(`${key}: ${value}`);
-    });
-    console.log(`Received Updater Key: ${apiKey}`);
-    console.log(UPDATER_KEY);
 
     const session = await getServerSession(authOptions);
     const isWm = session?.user.staffPositions.includes('WM');
