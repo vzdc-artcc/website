10874a3255efb76ce0043d4d71691fdfd3caf450
vainnor <me@carsonberget.com>
2026-01-13 17:50:21 -0600

    unused import

--- app/api/discord/link/route.ts
+++ app/api/discord/link/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/auth/auth";
 import { createDiscordAuthUrl } from "@/actions/discordLink";
-import type { NextRequest } from "next/server";
+
 
 export async function GET() {
     try {
