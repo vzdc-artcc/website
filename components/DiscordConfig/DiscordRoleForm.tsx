2b147f55f4df78dfd100882c56ea642263fb9cf9
vainnor <me@carsonberget.com>
2026-01-17 17:02:38 -0600

    added role items

---
+++ components/DiscordConfig/DiscordRoleForm.tsx
@@ -0,0 +1,55 @@
+'use client';
+import React from 'react';
+import {DiscordRole} from "@prisma/client";
+import {Grid2, TextField,} from "@mui/material";
+import {toast} from "react-toastify";
+import {useRouter} from "next/navigation";
+import FormSaveButton from "@/components/Form/FormSaveButton";
+import {upsertRole} from "@/actions/discordConfig";
+
+
+export default function DiscordRoleForm({role, discordConfigId}: { role?: DiscordRole, discordConfigId: string, }) {
+
+    const router = useRouter();
+    const handleSubmit = async (formData: FormData) => {
+
+        const {role: savedRole, errors} = await upsertRole(formData);
+        if (errors) {
+            toast(errors.map(e => e.message).join(".  "), {type: 'error'});
+            return;
+        }
+
+        toast(`Role '${savedRole.name}' saved successfully!`, {type: 'success'});
+
+        if (!role) {
+            router.push(`/web-system/discord-configs/role/new?discordConfigId=${discordConfigId}`);
+        }
+    }
+
+    return (
+        (<form action={handleSubmit}>
+            <input type="hidden" name="discordConfigId" value={discordConfigId} />
+            <input type="hidden" name="id" value={role?.id}/>
+            <Grid2 container columns={2} spacing={2}>
+                <Grid2
+                    size={{
+                        xs: 2,
+                        sm: 1
+                    }}>
+                    <TextField fullWidth variant="filled" label="Name" name="name" defaultValue={role?.name || ''}/>
+                </Grid2>
+                <Grid2
+                    size={{
+                        xs: 2,
+                        sm: 1
+                    }}>
+                    <TextField fullWidth variant="filled" label="Role ID" name="roleId" defaultValue={role?.roleId || ''}/>
+                </Grid2>
+                <Grid2 size={2}>
+                    <FormSaveButton />
+                </Grid2>
+            </Grid2>
+        </form>)
+    );
+
+}
