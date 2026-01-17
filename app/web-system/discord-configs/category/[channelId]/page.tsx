98fc05fcedecd8d1d5c607ecd5bfece640c534ca
vainnor <me@carsonberget.com>
2026-01-17 17:02:16 -0600

    added category items

---
+++ app/web-system/discord-configs/category/[channelId]/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import prisma from "@/lib/db";
+import {notFound} from "next/navigation";
+import {
+    Card,
+    CardContent,
+    Typography
+} from "@mui/material";
+import ChannelForm from "@/components/DiscordConfig/ChannelForm";
+
+export default async function Page(props: { params: Promise<{ channelId: string, }>, }) {
+    const params = await props.params;
+
+    const {channelId} = params;
+
+    const channel = await prisma.discordChannel.findUnique({
+        where: {
+            id: channelId,
+        },
+    });
+
+    if (!channel) {
+        notFound();
+    }
+
+    return (
+        <Card>
+            <CardContent>
+                <Typography variant="h5" sx={{mb: 2,}}>{channel.name}</Typography>
+                <ChannelForm discordConfigId={channel.discordConfigId}/>
+            </CardContent>
+        </Card>
+    );
+}
