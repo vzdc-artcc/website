58fd577ba6c275e1a9cf869f1019831d20b5d712
Aneesh Reddy <beabravedude@gmail.com>
2026-02-12 18:05:54 -0500

    Added files URL api.

---
+++ app/api/files/route.ts
@@ -0,0 +1,17 @@
+import prisma from "@/lib/db";
+
+export async function GET() {
+    const files = await prisma.file.findMany({
+        select: {
+            key: true,
+            name: true,
+        },
+    });
+
+    return Response.json(
+        files.map((f) => ({
+            ...f,
+            url: `https://utfs.io/f/${f.key}`,
+        }))
+    );
+}
